21c30d5b989b3f8d41d80c5bac3bc49c88f7f473
Tamira2804 <tamira2804@gmail.com>
2024-02-10 12:52:44 +0200

    implement filter logic

--- src/components/NanniesList/NanniesList.jsx
+++ src/components/NanniesList/NanniesList.jsx
@@ -1,16 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { List } from "./NanniesList.styled";
 import NannyCard from "components/NannyCard";
 import { Formik } from "formik";
+import { filterArray, renderSelected } from "helpers";
 
 const NanniesList = ({ nannies, filterValue }) => {
   const elementsOnPage = 3;
+  // console.log("nannies:", nannies);
+
+  const [loadedNannies, setLoadedNannies] = useState(elementsOnPage);
+  const filteredNannies = filterArray(nannies, filterValue);
+  const renderedNannies = renderSelected(filteredNannies, 0, loadedNannies);
+
+  // useEffect(() => {
+  //   if (nannies.length > 0 && nannies.length <= loadedNannies) {
+
+  //   }
+  // }, [])
 
   return (
     <Formik>
       <List>
-        {nannies.map((nanny) => (
-          <NannyCard key={nanny.id} nanny={nanny} />
+        {renderedNannies.map((nanny, index) => (
+          <NannyCard key={index} nanny={nanny} />
         ))}
       </List>
     </Formik>
